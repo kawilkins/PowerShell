6d26be4a99995c6555c4eac9cd9241bc8d68f32a
Kevin Wilkins <kwilkinsrd@gmail.com>
2025-11-28 09:48:50 -0600

    fix: Script header
    Updated script to use proper PowerShell script header.

--- GetWindowsUpdate.ps1
+++ GetWindowsUpdate.ps1
@@ -1,12 +1,33 @@
-﻿# Remember to set the execution policy using the following:
-#
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-#
+﻿<#
+.SYNOPSIS
+    Download and install Windows updates.
+
+.DESCRIPTION
+    Script will download and install Windows updates.
+    Additionally will also perform maintenance on the hard drive by
+
+        1) Analyzing drive C
+        2) Performing a defrag on drive C
+
+.AUTHOR
+    Kevin Wilkins
+    kwilkinsrd@gmail.com
+
+.CREATED
+    02/08/2019
+
+.VERSION
+    0.1.0
+
+.NOTES
+    Remember to set the execution policy to allow the script to run:
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#>
 
 # Set ErrorAction preference
 $ErrorActionPreference = 'SilentlyContinue'
 
-# Defrag HDD
+# Analyze and defrag HDD
 Optimize-Volume -DriveLetter C -Analyze -Defrag
 
 # Get and install windows updates
--- InstallPSWindowsUpdate.ps1
+++ InstallPSWindowsUpdate.ps1
@@ -1,7 +1,25 @@
-﻿# Remember to set the execution policy using the following:
-#
-# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-#
+﻿<#
+.SYNOPSIS
+    Install PSWindowsUpdate module in PowerShell.
+
+.DESCRIPTION
+    Script will install and import the PSWindowsUpdate module in PowerShell.
+    PSWindowsUpdate is essential for running Get-WindowsUpdate and Install-WindowsUpdate within PowerShell.
+
+.AUTHOR
+    Kevin Wilkins
+    kwilkinsrd@gmail.com
+
+.CREATED
+    02/08/2019
+
+.VERSION
+    0.1.0
+
+.NOTES
+    Remember to set the execution policy to allow the script to run.
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+#>
 
 # Set ErrorAction preference
 $ErrorActionPreference = 'SilentlyContinue'
