6402c397ccf65760005e781dd30d882352e42fcd
Kevin Wilkins <kwilkinsrd@gmail.com>
2025-12-03 12:19:22 -0600

    fix: Disable cryptsvc
    Add disabling of cryptsvc to completely shut it down.
    cryptsvc is enabled at end of script.

--- FixWinUpdateFail.ps1
+++ FixWinUpdateFail.ps1
@@ -17,15 +17,16 @@
     12/02/2025
 
 .VERSION
-    0.2.0
+    0.2.1
 
 .NOTES
     Remember to set the execution policy to allow the script to run.
     Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 #>
 
-# Set wuauserv to disabled so it does not auto restart
-Set-Service -Name wuauserv -StartupType Disabled
+# Set wuauserv and cryptsvc to disabled so it does not auto restart
+Set-Service -Name wuauserv -StartupType Disabled -Verbose
+Set-Service -Name cryptsvc -StartupType Disabled -Verbose
 
 # Stop Windows Update and related services
 $winUpdServs = "wuauserv","bits","cryptsvc","msiserver"
@@ -34,10 +35,11 @@ foreach ($service in $winUpdServs) {
 }
 
 # Recursively remove all items in SoftwareDistribution directory.
-Get-ChildItem -Path "C:\Windows\SoftwareDistribution\" -Recurse | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
+Get-ChildItem -Path "C:\Windows\SoftwareDistribution\" -Recurse -Verbose -ErrorAction SilentlyContinue | Remove-Item -Recurse -Force -Verbose -ErrorAction SilentlyContinue
 
-# Set wuauserv to start automatically
-Set-Service -Name wuauserv -StartupType Automatic
+# Set wuauserv and cryptsvc to start automatically
+Set-Service -Name wuauserv -StartupType Automatic -Verbose
+Set-Service -Name cryptsvc -StartupType Automatic -Verbose
 
 # Start Windows Update and related services
 foreach ($service in $winUpdServs) {
